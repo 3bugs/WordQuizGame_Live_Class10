79340e6a08a4c357d2efee47682f6e7420caf5e0
Promlert Lovichit <promlert@gmail.com>
2015-11-01 11:33:15 +0700

    Add GameActivity

--- app/src/main/java/com/example/wordquizgame/MainActivity.java
+++ app/src/main/java/com/example/wordquizgame/MainActivity.java
@@ -1,31 +1,40 @@
 package com.example.wordquizgame;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button playGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = (Button) findViewById(R.id.playGameButton);
-        b.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast t1 = Toast.makeText(
-                MainActivity.this,
-                "Hello",
-                Toast.LENGTH_SHORT
-        );
-
-        t1.show();
+        playGameButton = (Button) findViewById(R.id.playGameButton);
+        playGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("เลือกระดับความยาก");
+                dialog.setItems(
+                        new String[]{"ง่าย", "ปานกลาง", "ยาก"},
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i(TAG, "ผู้ใช้เลือก " + which);
+                            }
+                        }
+                );
+                dialog.show();
+            }
+        });
     }
 }
