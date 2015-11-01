83fd32c4a6e9b1cc0efe2021402a5996a846bcf1
Promlert Lovichit <promlert@gmail.com>
2015-11-01 09:33:13 +0700

    End of first day

--- app/src/main/java/com/example/wordquizgame/MainActivity.java
+++ app/src/main/java/com/example/wordquizgame/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.wordquizgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.playGameButton);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast t1 = Toast.makeText(
+                MainActivity.this,
+                "Hello",
+                Toast.LENGTH_SHORT
+        );
+
+        t1.show();
     }
 }
